de55fdd9c14b9745652f0ee441e79aafee9de61a
SomberTM <sombertm@gmail.com>
2023-09-17 23:21:26 -0400

    No posts indicator

--- src/app/(media)/posts/[page]/page.tsx
+++ src/app/(media)/posts/[page]/page.tsx
@@ -14,24 +14,29 @@ export default async function Posts({ params }: { params: { page: string } }) {
 
   return (
     <Centered className="p-8">
-      <Card className="w-96 md:w-1/2 p-4 flex flex-col gap-4">
-        <ul className="space-y-2">
-          {results.map(({ posts: post, user: author }) => (
-            <li key={post.id}>
-              <Post post={post} author={author!} />
-            </li>
-          ))}
-        </ul>
-        <div className="flex justify-between">
-          <Button variant="link" disabled={page === 1}>
-            <Link href={`/posts/${page - 1}`}>Previous</Link>
-          </Button>
+      {results.length === 0 && (
+        <h1 className="text-3xl font-bold text-primary">No posts yet!</h1>
+      )}
+      {results.length > 0 && (
+        <Card className="w-96 md:w-1/2 p-4 flex flex-col gap-4">
+          <ul className="space-y-2">
+            {results.map(({ posts: post, user: author }) => (
+              <li key={post.id}>
+                <Post post={post} author={author!} />
+              </li>
+            ))}
+          </ul>
+          <div className="flex justify-between">
+            <Button variant="link" disabled={page === 1}>
+              <Link href={`/posts/${page - 1}`}>Previous</Link>
+            </Button>
 
-          <Button variant="link" disabled={!hasNextPage}>
-            <Link href={`/posts/${page + 1}`}>Next</Link>
-          </Button>
-        </div>
-      </Card>
+            <Button variant="link" disabled={!hasNextPage}>
+              <Link href={`/posts/${page + 1}`}>Next</Link>
+            </Button>
+          </div>
+        </Card>
+      )}
     </Centered>
   );
 }
