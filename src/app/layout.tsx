ca4163f9e695174f86533eb61869c20b91f477ec
SomberTM <sombertm@gmail.com>
2023-09-22 22:35:29 -0400

    Bitch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,10 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Suspense } from "react";
 
+import "@/styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,9 +24,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn(inter.className, "flex flex-col min-h-screen")}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {/* <Toaster /> */}
+          <Toaster />
           <Nav />
-          <main className="grow flex flex-col">{children}</main>
+          <main className="grow flex flex-col">
+            <Suspense>{children}</Suspense>
+          </main>
         </ThemeProvider>
       </body>
     </html>
